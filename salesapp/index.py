38cd56cfb8a0a5e5ecb05e456118daaffbaa7d59
Khiem <wert.kb@gmail.com>
2023-09-17 23:58:22 +0700

    update v3

--- salesapp/index.py
+++ salesapp/index.py
@@ -12,10 +12,17 @@ def home():
 @app.route("/products")
 def product_list():
     cate_id = request.args.get("category_id")
+    kw = request.args.get("keyword")
+    from_price = request.args.get("from_price")
+    to_price = request.args.get("to_price")
 
-    products = utils.load_products(cate_id=cate_id)
+    products = utils.load_products(cate_id=cate_id, kw=kw, from_price=from_price, to_price=to_price)
 
     return render_template('products.html', products=products)
+
+@app.route("/products/<int:product_id>")
+def product_detail(product_id):
+    products = utils.get_product_by_id(product_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- salesapp/utils.py
+++ salesapp/utils.py
@@ -14,4 +14,19 @@ def load_products(cate_id=None, kw=None, from_price=None, to_price=None):
     if cate_id:
         products = [p for p in products if p['category_id'] == int(cate_id)]
 
+    if kw:
+        products = [p for p in products if p['name'].lower().find(kw.lower()) >= 0]
+    if from_price:
+        products = [p for p in products if p['price'] >= float(from_price)]
+    if to_price:
+        products = [p for p in products if p['price'] >= float(to_price)]
+
     return products
+
+def get_product_by_id(product_id):
+    products = read_json(os.path.join(app.root_path, 'data/products.json'))
+
+    for p in products:
+        if p['id'] == product_id:
+            return p
+    return None
